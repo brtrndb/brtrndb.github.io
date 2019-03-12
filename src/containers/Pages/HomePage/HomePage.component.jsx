c3e09ee710aa7239df4bd68acc5bb8226fac7e8e
Bertrand B <boyer.bertrand@gmail.com>
2019-03-12 11:02:19 +0100

    Use of Typography and Grid for styling HomePage welcome message.

--- src/containers/Pages/HomePage/HomePage.component.jsx
+++ src/containers/Pages/HomePage/HomePage.component.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
-import { HomePageContainer } from './HomePage.style';
+import { HomePageContainer, WelcomeContainer } from './HomePage.style';
 import { messages } from './HomePage.i18n';
 
 const HomePage = () => (
   <HomePageContainer>
-    <FormattedMessage id={messages.welcome} />
+    <WelcomeContainer>
+      <Grid container direction='column' justify='center' alignItems='center' alignContent='center'>
+        <Grid item>
+          <Typography variant='h2'>
+            <FormattedMessage id={messages.welcome} />
+          </Typography>
+        </Grid>
+      </Grid>
+    </WelcomeContainer>
   </HomePageContainer>
 );
 
--- src/containers/Pages/HomePage/HomePage.style.jsx
+++ src/containers/Pages/HomePage/HomePage.style.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const DefaultContainer = styled.div``;
 const HomePageContainer = styled.div``;
+const WelcomeContainer = styled.div``;
 
-export { HomePageContainer };
+export { HomePageContainer, WelcomeContainer };
 export default DefaultContainer;
