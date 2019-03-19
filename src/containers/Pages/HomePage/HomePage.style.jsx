05554b29dc9a298687f4550ba1a54ffca4983ffa
Bertrand B <boyer.bertrand@gmail.com>
2019-03-19 15:55:16 +0100

    Fix warning.

--- src/containers/Pages/HomePage/HomePage.style.jsx
+++ src/containers/Pages/HomePage/HomePage.style.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const DefaultContainer = styled.div``;
 const HomePageContainer = styled.div``;
 
-const styles = (theme) => ({
+const styles = () => ({
   mainGrid: {
     height: '40vh'
   }
