15a52a0945ffeb0f649f61d3cb4d0839d8d4e01f
Bertrand B <boyer.bertrand@gmail.com>
2019-03-19 11:19:04 +0100

    Added reducers file for intl module.

--- src/modules/intl/ducks/index.jsx
+++ src/modules/intl/ducks/index.jsx
@@ -1,7 +1,7 @@
-import { intlReducer } from 'react-intl-redux';
+import reducers from './reducers';
 
 export { default as intlInitialState } from './initialState';
 export { default as intlSelectors } from './selectors';
 export { default as intlOperations } from './operations';
 
-export default intlReducer;
+export default reducers;
---
+++ src/modules/intl/ducks/reducers.jsx
@@ -0,0 +1,7 @@
+import { intlReducer } from 'react-intl-redux';
+
+const reducers = {
+  intl: intlReducer
+};
+
+export default reducers;
