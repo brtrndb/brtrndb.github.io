28afeb4c77c48b6a969536eff08297aeda3e0206
Bertrand B <boyer.bertrand@gmail.com>
2019-03-19 16:46:46 +0100

    Code refactoring.

--- src/containers/Pages/HomePage/HomePage.component.jsx
+++ src/containers/Pages/HomePage/HomePage.component.jsx
@@ -8,6 +8,12 @@ import { withStyles } from '@material-ui/core/styles';
 import { styles, HomePageContainer } from './HomePage.style';
 import { messages } from './HomePage.i18n';
 
+const toSubtitle = (msg) => (
+  <Grid key={msg} item>
+    <FormattedMessage id={msg} />
+  </Grid>
+);
+
 const HomePage = ({ classes }) => (
   <HomePageContainer>
     <Grid container direction='column' justify='center' alignItems='center' alignContent='center' className={classes.mainGrid}>
@@ -17,17 +23,9 @@ const HomePage = ({ classes }) => (
         </Typography>
       </Grid>
       <Grid item>
-        <Typography variant='subtitle1' align='center'>
-          <Grid container direction='row' justify='center' alignItems='center' alignContent='center' spacing={8}>
-            {[messages.my_name, messages.i_am, messages.i_try].map((message) => (
-              <Grid key={message} item>
-                <Typography>
-                  <FormattedMessage id={message} />
-                </Typography>
-              </Grid>
-            ))}
-          </Grid>
-        </Typography>
+        <Grid container direction='row' justify='center' alignItems='center' alignContent='center' spacing={8}>
+          <Typography variant='subtitle1'>{[messages.my_name, messages.i_am, messages.i_try].map(toSubtitle)}</Typography>
+        </Grid>
       </Grid>
     </Grid>
   </HomePageContainer>
