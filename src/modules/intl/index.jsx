977c255c548c4dd5ba401ca27c5cc38e1b65570c
Bertrand B <boyer.bertrand@gmail.com>
2019-03-20 15:32:08 +0100

    Now allows locale FR.

--- src/modules/intl/index.jsx
+++ src/modules/intl/index.jsx
@@ -1,9 +1,11 @@
 import { addLocaleData } from 'react-intl';
 import enLocaleData from 'react-intl/locale-data/en';
+import frLocaleData from 'react-intl/locale-data/fr';
 
 import i18n from './i18n';
 
 addLocaleData(enLocaleData);
+addLocaleData(frLocaleData);
 
 export { intlInitialState } from './ducks';
 export { intlSelectors } from './ducks';
--- src/modules/intl/locales.jsx
+++ src/modules/intl/locales.jsx
@@ -1,4 +1,4 @@
-const ALLOWED_LOCALES = ['en'];
+const ALLOWED_LOCALES = ['en', 'fr'];
 const DEFAULT_LOCALE = 'en';
 const USER_LOCALE = navigator.language.toLowerCase().split(/[_-]+/)[0];
 const INITIAL_LOCALE = ALLOWED_LOCALES.includes(USER_LOCALE) ? USER_LOCALE : DEFAULT_LOCALE;
