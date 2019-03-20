579674c9f63dcab221d8a51fbe5f1758498ebb86
Bertrand B <boyer.bertrand@gmail.com>
2019-03-20 15:40:31 +0100

    Integrate Header to the app.

--- src/containers/AppContent/AppContent.component.jsx
+++ src/containers/AppContent/AppContent.component.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Routes from 'Containers/Routes';
 import Helmet from 'Components/Helmet';
+import Header from 'Components/Header';
 
 import { AppContentContainer } from './AppContent.style';
 
 const AppContent = () => (
   <AppContentContainer>
     <Helmet />
+    <Header />
     <Routes baseurl='/' />
   </AppContentContainer>
 );
--- src/modules/intl/i18n.jsx
+++ src/modules/intl/i18n.jsx
@@ -1,8 +1,9 @@
+import i18nHeader from 'Components/Header/Header.i18n';
 import i18nHomePage from 'Containers/Pages/HomePage/HomePage.i18n';
 
 import locales from './locales';
 
-const i18nModules = [i18nHomePage];
+const i18nModules = [i18nHeader, i18nHomePage];
 
 const i18n = locales.ALLOWED_LOCALES.reduce((acc, locale) => {
   const localeMessages = i18nModules.reduce((messages, module) => ({ ...messages, ...module[locale] }), {});
