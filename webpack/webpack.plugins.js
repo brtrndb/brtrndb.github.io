9962dac74368257f0ecc18eaf738aff31cbe1b2b
Bertrand B <boyer.bertrand@gmail.com>
2019-03-25 12:29:45 +0100

    Added favicon generation on build with FaviconsWebpackPlugin.

--- webpack/webpack.plugins.js
+++ webpack/webpack.plugins.js
@@ -10,6 +10,7 @@ const OfflinePlugin = require('offline-plugin');
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = (params) => {
   const plugins = [];
@@ -79,6 +80,32 @@ module.exports = (params) => {
           destination: 'icons'
         }
       ]
+    })
+  );
+
+  plugins.push(
+    new FaviconsWebpackPlugin({
+      logo: path.join(params.paths.root, 'webpack/brtrndb-icon.png'), // Your source logo
+      prefix: 'icons/', // The prefix for all image files (might be a folder or a name).
+      // emitStats: false, // Emit all stats of the generated icons.
+      // statsFilename: 'iconstats.json', // The name of the json containing all favicon information.
+      persistentCache: false, // Generate a cache file with control hashes and don't rebuild the favicons until those hashes change.
+      inject: false, // Inject the html into the html-webpack-plugin.
+      // background: '#ffffff', // favicon background color (see https://github.com/haydenbleasel/favicons#usage).
+      // title: 'Webpack App', // favicon app title (see https://github.com/haydenbleasel/favicons#usage).
+      icons: {
+        // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage).
+        android: false,
+        appleIcon: false,
+        appleStartup: false,
+        coast: false,
+        favicons: true,
+        firefox: false,
+        opengraph: false,
+        twitter: false,
+        yandex: false,
+        windows: false
+      }
     })
   );
 
