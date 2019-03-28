c37924acad26d1c7f6981dc237daa00e06b0cf56
Bertrand B <boyer.bertrand@gmail.com>
2019-03-28 17:06:15 +0100

    Added a messageDescriptorPropTypes to intl module.

--- src/modules/intl/index.jsx
+++ src/modules/intl/index.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export { intlInitialState } from './ducks';
 export { intlSelectors } from './ducks';
 export { intlOperations } from './ducks';
@@ -5,5 +7,13 @@ export { default as intlReducers } from './ducks';
 
 export { default as getUserLocale } from './getUserLocale';
 export { default as setupIntl } from './setupIntl';
+
+const messageDescriptorPropTypes = PropTypes.shape({
+  id: PropTypes.string.isRequired,
+  description: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  defaultMessage: PropTypes.string
+});
+
+export { messageDescriptorPropTypes };
 
 export default {};
