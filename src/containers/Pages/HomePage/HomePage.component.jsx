eb51aa158d34f70dd457a643a092084d9eafb0ca
Bertrand B <boyer.bertrand@gmail.com>
2019-03-29 14:57:02 +0100

    Removed usused imports.

--- src/containers/Pages/HomePage/HomePage.component.jsx
+++ src/containers/Pages/HomePage/HomePage.component.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
 
 import messages from './HomePage.i18n';
 import { HomePageContainer } from './HomePage.style';
