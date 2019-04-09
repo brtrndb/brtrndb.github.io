5b20463bde6d00ae54913fc9cb31992e156f920e
Bertrand B <boyer.bertrand@gmail.com>
2019-04-09 13:15:35 +0200

    Added minor code documentation.

--- src/utils/createReduxAction.jsx
+++ src/utils/createReduxAction.jsx
@@ -1,3 +1,9 @@
+/**
+ * Create an action for the Redux store .
+ * @function
+ * @param {string} type - The action type, generally formatted as 'COMPONENT/ACTION_NAME'.
+ * @param {object} payload - The action payload.
+ */
 const createReduxAction = (type, payload) => ({ type, payload });
 
 export default createReduxAction;
